15a0040e418e98c0277e6f142936951c98039e07
hbwhyte <heather.b.whyte@gmail.com>
2020-12-26 21:56:01 +0000

    Sets up basic csv reading

--- bre/bre.py
+++ bre/bre.py
@@ -1,2 +1,25 @@
+import csv
+
+
+def init():
+    with open("data/sample.csv") as story_file:
+        csv_reader = csv.reader(story_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+                print(f'Page {row[0]} scenario is: {row[1]}')
+                line_count += 1
+        print(f'Processed {line_count} pages')
+
+
+def start_game():
+    print("Start the game!")
+    init()
+    print("Game over!")
+
+
 if __name__ == '__main__':
-    pass
+    start_game()
