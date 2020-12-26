f91d1fbc86a2467b3bfad368fd35fd226fd34f11
hbwhyte <heather.b.whyte@gmail.com>
2020-12-26 22:36:27 +0000

    Setup classes for page setup

--- bre/bre.py
+++ bre/bre.py
@@ -1,23 +1,38 @@
 import csv
 
 
+class Page:
+    def __init__(self, page, text, choices):
+        self.page = page
+        self.text = text
+        self.choices = choices
+
+
+class Choice:
+    def __init__(self, choice, next_page):
+        self.choice = choice
+        self.next_page = next_page
+
+
 def init():
     with open("data/sample.csv") as story_file:
         csv_reader = csv.reader(story_file, delimiter=',')
-        line_count = 0
+        next(csv_reader, None)
+        pages = {}
+        first_page = "Not Set Yet"
         for row in csv_reader:
-            if line_count == 0:
-                print(f'Column names are {", ".join(row)}')
-                line_count += 1
-            else:
-                print(f'Page {row[0]} scenario is: {row[1]}')
-                line_count += 1
-        print(f'Processed {line_count} pages')
+            if first_page == "Not Set Yet":
+                first_page = row[0]
+            choice1 = Choice(row[2], row[3])
+            choice2 = Choice(row[4], row[5])
+            pages[row[0]] = Page(row[0], row[1], [choice1, choice2])
+        return pages[first_page]
 
 
 def start_game():
     print("Start the game!")
-    init()
+    first_page = init()
+    print(first_page.text)
     print("Game over!")
 
 
