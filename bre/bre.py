b76f7ec7a0a6045a236a051c5b5d522d1bde306b
hbwhyte <heather.b.whyte@gmail.com>
2020-12-26 23:21:40 +0000

    Loop through choices

--- bre/bre.py
+++ bre/bre.py
@@ -1,6 +1,12 @@
 import csv
 
 
+class Story:
+    def __init__(self, pages, current_page):
+        self.pages = pages
+        self.current_page = current_page
+
+
 class Page:
     def __init__(self, page, text, choices):
         self.page = page
@@ -26,14 +32,36 @@ def init():
             choice1 = Choice(row[2], row[3])
             choice2 = Choice(row[4], row[5])
             pages[row[0]] = Page(row[0], row[1], [choice1, choice2])
-        return pages[first_page]
+        story = Story(pages, first_page)
+        return story
 
 
 def start_game():
     print("Start the game!")
-    first_page = init()
-    print(first_page.text)
+    story = init()
+    next_page = show_options(story.pages[story.current_page])
+    while next_page is not '':
+        next_page = show_options(story.pages[next_page])
     print("Game over!")
+
+
+def show_options(page):
+    print(page.page)
+    print('----')
+    print(page.text+'\n')
+    print(f'1. {page.choices[0].choice}')
+    print(f'2. {page.choices[1].choice}')
+
+    while True:
+        try:
+            choice = input('Which do you choose? (1/2) ')
+            if choice == '1':
+                return page.choices[0].next_page
+            if choice == '2':
+                return page.choices[1].next_page
+            print("Invalid response entered. Do you choose 1 or 2? ")
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
